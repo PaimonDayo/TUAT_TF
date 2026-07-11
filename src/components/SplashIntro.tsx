741cbe13f9e71d59067404e1da43924e4d79f711
PaimonDayo <rainbowrdayo@gmail.com>
2026-07-12 07:47:09 +0900

    Restore source splash frame and typography

--- src/components/SplashIntro.tsx
+++ src/components/SplashIntro.tsx
@@ -71,10 +71,30 @@ export default function SplashIntro() {
         </section>
         <section className={`${styles.scene} ${styles.final}`}>
           <svg className={styles.frame} viewBox="0 0 1600 900" preserveAspectRatio="xMidYMid meet">
-            <path d="M110 275 C300 205 505 254 748 225 S1255 195 1490 286" />
-            <path d="M130 650 C440 710 705 648 930 692 S1300 685 1480 600" />
-            <path d="M125 274 C102 430 110 548 143 650" />
-            <path d="M1488 287 C1515 420 1505 522 1478 600" />
+            <defs>
+              <filter id="splash-frame-rough" x="-6%" y="-6%" width="112%" height="112%">
+                <feTurbulence type="fractalNoise" baseFrequency="0.013 0.022" numOctaves="2" seed="8" />
+                <feDisplacementMap in="SourceGraphic" scale="11" />
+              </filter>
+              <filter id="splash-frame-brush" x="-8%" y="-8%" width="116%" height="116%">
+                <feTurbulence type="fractalNoise" baseFrequency="0.012 0.02" numOctaves="2" seed="6" result="noise" />
+                <feDisplacementMap in="SourceGraphic" in2="noise" scale="10" />
+              </filter>
+              <linearGradient id="splash-line-color" gradientUnits="userSpaceOnUse" x1="0" y1="0" x2="150" y2="95" spreadMethod="repeat">
+                <stop offset="0" stopColor="#6a48c4" /><stop offset=".16" stopColor="#3f7ae0" />
+                <stop offset=".28" stopColor="#8a5ad0" /><stop offset=".4" stopColor="#e878c0" />
+                <stop offset=".5" stopColor="#47c4e8" /><stop offset=".62" stopColor="#5a3fb4" />
+                <stop offset=".74" stopColor="#d84a9c" /><stop offset=".84" stopColor="#4a8ae4" />
+                <stop offset=".93" stopColor="#f0ecf6" /><stop offset="1" stopColor="#6a48c4" />
+              </linearGradient>
+            </defs>
+            <g filter="url(#splash-frame-rough)" transform="rotate(-1 800 460)">
+              <path className={styles.frame1} d="M 445 345 L 900 300 L 1145 285 M 1160 288 L 1245 330 L 1232 560 M 1228 566 L 1060 640 L 700 660 M 640 662 L 520 668 L 400 600 L 418 402 L 442 350" />
+              <path className={styles.frame2} d="M 430 358 L 890 312 M 1150 298 L 1236 342 M 1222 574 L 1052 652 L 530 678 M 408 610 L 426 408" />
+              <path className={styles.frame3} d="M 292 372 L 560 322 M 1108 246 L 1338 356 M 1296 252 L 1172 368 M 1258 276 L 1238 630" />
+              <path className={styles.frame4} d="M 252 778 L 388 742 M 402 738 L 560 700 M 300 828 L 452 788 M 470 782 L 618 742" />
+              <path className={styles.frame5} d="M 452 336 L 1148 276 M 1240 336 L 1226 556" />
+            </g>
           </svg>
           <div className={styles.title}>TUAT</div>
           <div className={styles.credit}>Track / Field</div>
