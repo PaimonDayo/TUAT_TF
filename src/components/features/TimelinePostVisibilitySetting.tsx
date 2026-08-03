479907d676d00895fc62378d7f7919a5d726aa90
PaimonDayo <rainbowrdayo@gmail.com>
2026-08-04 00:10:20 +0900

    Fix timeline setting label encoding

--- src/components/features/TimelinePostVisibilitySetting.tsx
+++ src/components/features/TimelinePostVisibilitySetting.tsx
@@ -48,8 +48,8 @@ export function TimelinePostVisibilitySetting({
   return (
     <Toggle
       variant="row"
-      label="?????????????????"
-      description="??????????????????????????????????????"
+      label={"\u30bf\u30a4\u30e0\u30e9\u30a4\u30f3\u306b\u81ea\u5206\u306e\u6295\u7a3f\u3092\u8868\u793a\u3059\u308b"}
+      description={"\u30aa\u30d5\u306b\u3059\u308b\u3068\u3001\u7df4\u7fd2\u8a18\u9332\u3068\u3064\u3076\u3084\u304d\u3092\u30db\u30fc\u30e0\u30fb\u30bf\u30a4\u30e0\u30e9\u30a4\u30f3\u306e\u4e00\u89a7\u306b\u8868\u793a\u3057\u307e\u305b\u3093\u3002"}
       checked={visible}
       disabled={busy}
       onChange={toggle}
